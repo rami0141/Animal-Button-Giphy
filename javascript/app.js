0675f033c3f5e4e31a965898a8d3f10b14597cee
Cristina Zhang <rami0141@umn.edu>
2018-01-02 19:00:08 -0600

    more code

--- javascript/app.js
+++ javascript/app.js
@@ -2,22 +2,63 @@
 var animals = ["dog", "cat", "bird", "fish", "lion", "wolf", "snakes", "turtle", "fox", "lizard", "elephant", 
 "chicken", "cow", "pig"];
 
-//Display Giphy
-function showGiphy() {
-	var animal = $(this).attr("data-name");
-	var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=WhkiKuWXUxXZ3GTPlYBDiyRjqFagaPpt&tag=&rating=G";
 
-	$.ajax({
-		url: queryURL,
-		method: "GET"
-	}) 
-}
+//Giphy
+
+ $("#animalButtons").on("click", function() {
+ 	$("#animals").empty();
+      // Grabbing and storing the data-animal property value from the button
+      var species = $(this).attr("data-name");
+ 
+      // Constructing a queryURL using the animal name
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        species + "&api_key=WhkiKuWXUxXZ3GTPlYBDiyRjqFagaPpt&q=animals&limit=10&offset=0&rating=G&lang=en";
+
+      // Performing an AJAX request with the queryURL
+      $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+        // After data comes back from the request
+        .done(function(response) {
+          console.log(queryURL);
+
+          console.log(response);
+          // storing the data from the AJAX request in the results variable
+          var results = response.data;
+
+          // Looping through each result item
+          for (var i = 0; i < results.length; i++) {
+
+            // Creating and storing a div tag
+            var animalDiv = $("<div>");
+
+            // Creating a paragraph tag with the result item's rating
+            var p = $("<p>").text("Rating: " + results[i].rating);
+
+            // Creating and storing an image tag
+            var animalImage = $("<img>");
+            // Setting the src attribute of the image to a property pulled off the result item
+            animalImage.attr("src", results[i].images.fixed_height.url);
+
+            // Appending the paragraph and image tag to the animalDiv
+            animalDiv.append(p);
+            animalDiv.append(animalImage);
+
+            // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+            $("#animals").prepend(animalDiv);
+          }
+        });
+    });	
+
+
 
 
 
 // Creating Buttons
 
 function creatingButtons() {
+	$("#animalButtons").empty();
 	for (var i = 0; i < animals.length; i++) {
 
 		var a = $("<button>");
@@ -29,9 +70,9 @@ function creatingButtons() {
 		a.text(animals[i]);
 
 		$("#animalButtons").append(a);
-
 	}
 }
+
 
 $("#addAnimal").on("click", function(event) {
 	event.preventDefault();
@@ -41,6 +82,7 @@ $("#addAnimal").on("click", function(event) {
 	animals.push(animal);
 
 	creatingButtons();
+
 });
 
-creatingButtons();
+	creatingButtons();
