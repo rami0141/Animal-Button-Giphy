77f12571d07895059c572fc61897934b8ded15b1
Cristina Zhang <rami0141@umn.edu>
2018-01-03 20:28:13 -0600

    more JavaScript code

--- javascript/app.js
+++ javascript/app.js
@@ -3,62 +3,61 @@ var animals = ["dog", "cat", "bird", "fish", "lion", "wolf", "snakes", "turtle",
 "chicken", "cow", "pig"];
 
 
-//Giphy
+// Delegated event
+$(document).on('click', ".animal", giphy); 
 
- $("#animalButtons").on("click", function() {
- 	$("#animals").empty();
+creatingButtons();
+
+
+// Giphy
+ function giphy() {
+    $("#animals").empty();
+
       // Grabbing and storing the data-animal property value from the button
       var species = $(this).attr("data-name");
- 
-      // Constructing a queryURL using the animal name
+
       var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        species + "&api_key=WhkiKuWXUxXZ3GTPlYBDiyRjqFagaPpt&q=animals&limit=10&offset=0&rating=G&lang=en";
+        species + "&api_key=WhkiKuWXUxXZ3GTPlYBDiyRjqFagaPpt&limit=10&offset=0&rating=G&lang=en";
 
       // Performing an AJAX request with the queryURL
       $.ajax({
           url: queryURL,
           method: "GET"
         })
-        // After data comes back from the request
-        .done(function(response) {
-          console.log(queryURL);
 
-          console.log(response);
-          // storing the data from the AJAX request in the results variable
+        .done(function(response) {
+          
           var results = response.data;
 
           // Looping through each result item
           for (var i = 0; i < results.length; i++) {
 
-            // Creating and storing a div tag
             var animalDiv = $("<div>");
 
-            // Creating a paragraph tag with the result item's rating
             var p = $("<p>").text("Rating: " + results[i].rating);
 
-            // Creating and storing an image tag
             var animalImage = $("<img>");
-            // Setting the src attribute of the image to a property pulled off the result item
+            
             animalImage.attr("src", results[i].images.fixed_height.url);
+            animalImage.addClass("aimage");
 
-            // Appending the paragraph and image tag to the animalDiv
+            //animalImage.attr("id", i);
+           
             animalDiv.append(p);
             animalDiv.append(animalImage);
 
-            // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
             $("#animals").prepend(animalDiv);
+
           }
         });
-    });	
-
-
+    };	
 
 
 
 // Creating Buttons
 
 function creatingButtons() {
-	$("#animalButtons").empty();
+	$(".animalButtons").empty();
 	for (var i = 0; i < animals.length; i++) {
 
 		var a = $("<button>");
@@ -69,10 +68,10 @@ function creatingButtons() {
 
 		a.text(animals[i]);
 
-		$("#animalButtons").append(a);
+		$(".animalButtons").append(a);
 	}
-}
 
+}
 
 $("#addAnimal").on("click", function(event) {
 	event.preventDefault();
@@ -82,7 +81,17 @@ $("#addAnimal").on("click", function(event) {
 	animals.push(animal);
 
 	creatingButtons();
+  $("#animals").empty();
 
 });
 
+
+
+// Pause and Play Giphy
+
+$(document).on('mouseover', ".aimage", function (){
+  $(".aimage").mouseover(function(){
+    $(this).attr("src", "https://media2.giphy.com/media/jSqZDS0LhmHwk/200_s.gif");
+  });
+});
 	creatingButtons();
